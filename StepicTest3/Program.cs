21119fce49a84048ce250947bc4bd5f7fbad21f2
Andrew-Tula <troyka-service-director@yandex.ru>
2023-01-12 15:10:21 +0300

    Добавьте файлы проекта.

---
+++ StepicTest3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+public static class myprogram
+{
+    static int Do(int a, int b) => a * b;
+    static int Do(double a, int b) => (int)(a / b);
+    public static void Main()
+    {
+        float a = 231;
+        int b = '2';
+        Console.WriteLine(Do(a, b));
+    }
+}
